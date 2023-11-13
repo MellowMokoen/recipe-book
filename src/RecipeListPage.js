718f09aff266357a701c9563c717112b3c420f35
MellowMokoen <mamello3632@gmail.com>
2023-11-13 12:37:03 +0200

    Added the im container and styled row

--- src/RecipeListPage.js
+++ src/RecipeListPage.js
@@ -12,25 +12,79 @@ import stake from './images/stake.jpg';
 
 export default function() {
     return (
-        <div>
-            <h1>Find your favorite meal and get to the kitchen</h1>
-         <div className = "row">
-            <div className="col-md-4 beef-row">
-            <img src= {lasagne} alt="lasagne"/>
-            <img src= {beef} alt="beef burger"/>
-            <img src= {rice}  alt="rice tomatoes"/>
-            </div>
-            <div className="col-md-4 pizza-row">
-            <img src= {pizza} alt="pizza"/>
-            <img src= {stake} alt="beef stake"/>
-            <img src= {cheese} alt="cheese burger"/>
-            </div>
-            <div className="col-md-4 chicken-row">
-            <img src= {noodles} alt="noodles"/>
-            <img src= {pasta} alt="chicken pasta"/>
-            <img src= {chicken} alt="full roasted chicken"/>
-            </div>
+    <div>
+        <h1>Find your favorite meal and get to the kitchen</h1>
+         
+      <div className="row">
+        <div className="col-md-4">
+          <div className="image-container">
+             <h3>Cheesy lasagne</h3>
+            <img src={lasagne} alt="lasagne" />
+          </div>
+        </div>
+
+        <div className="col-md-4">
+          <div className="image-container">
+             <h3>Beef Burger</h3>
+            <img src={beef} alt="beef burger" />
+          </div>
+        </div>
+
+        <div className="col-md-4">
+          <div className="image-container">
+            <h3>Rice Tomatoes</h3>
+            <img src={rice} alt="rice tomatoes" />
+          </div>
+        </div>
+      </div>
+
+      <div className="row">
+        <div className="col-md-4">
+         <div className="image-container">
+            <h3>Cheese Burger</h3>
+           <img src={cheese} alt="Cheese burger" />
+          </div>
+        </div>
+
+       <div className="col-md-4">
+        <div className="image-container">
+          <h3>Chicken Pasta</h3>
+          <img src={pasta} alt="Chicken Pasta" />
+        </div>
+       </div>
+
+       <div className="col-md-4">
+        <div className="image-container">
+          <h3>Noodles</h3>
+          <img src={noodles} alt="Noodles" />
+        </div>
+       </div>
+      </div>
+
+      <div className="row">
+        <div className="col-md-4">
+         <div className="image-container">
+           <h3>Mushroom Pizza</h3>
+           <img src={pizza} alt="Mushroom Pizza" />
          </div>
         </div>
-    );
+
+       <div className="col-md-4">
+          <div className="image-container">
+            <h3>Roasted chicken</h3>
+            <img src={chicken} alt="Full roasted chicken" />
+          </div>
+        </div>
+
+        <div className="col-md-4">
+          <div className="image-container">
+             <h3>Beef Stake</h3>
+             <img src={stake} alt="rice tomatoes" />
+          </div>
+        </div>
+      </div>
+      
+    </div>
+  );
+
 }
